efefdfc3097905df8117674066f563d8685fc587
Wieberen Jonker <wieberen@paypro.nl>
2019-01-24 12:38:04 +0100

    Use sanitize_text_field

--- paypro-gateways-woocommerce/includes/paypro/wc/plugin.php
+++ paypro-gateways-woocommerce/includes/paypro/wc/plugin.php
@@ -4,7 +4,7 @@ class PayPro_WC_Plugin
 {
     const PLUGIN_ID = 'paypro-gateways-woocommerce';
     const PLUGIN_TITLE = 'PayPro Gateways - WooCommerce';
-    const PLUGIN_VERSION = '1.3.1';
+    const PLUGIN_VERSION = '1.3.2';
 
     public static $paypro_gateways = array(
         'PayPro_WC_Gateway_Ideal',
